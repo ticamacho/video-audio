ca429c620361fb4a5f583023a9653dd06652ee38
Tiago Camacho <tiago.miti@gmail.com>
2025-09-18 17:17:12 +1000

    add: card component

---
+++ components/card.tsx
@@ -0,0 +1,38 @@
+import { cn } from "../utils";
+
+interface CardProps {
+  className?: string;
+  children?: React.ReactNode;
+}
+
+function Root({ children, className }: CardProps) {
+  return (
+    <div
+      className={cn(
+        "shadow-[1px 3px 12px 0 rgba(0, 0, 0, 0.03)] overflow-hidden rounded-2xl bg-white",
+        className,
+      )}
+    >
+      {children}
+    </div>
+  );
+}
+
+function Content({ children, className }: CardProps) {
+  return <div className={cn("bg-white px-6 pt-6", className)}>{children}</div>;
+}
+
+function Actions({ children, className }: CardProps) {
+  return (
+    <div
+      className={cn(
+        "flex items-center justify-end gap-2 px-6 pt-8 pb-6",
+        className,
+      )}
+    >
+      {children}
+    </div>
+  );
+}
+
+export { Root, Content, Actions };
--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,6 @@ import VideoLayout from "./video-layout";
 import ControlBar from "./control-bar";
 import ParticipantTile from "./participant-tile";
 import Indicator from "./indicator";
+import * as Card from "./card";
 
-export { VideoLayout, ControlBar, ParticipantTile, Indicator };
+export { VideoLayout, ControlBar, ParticipantTile, Indicator, Card };
