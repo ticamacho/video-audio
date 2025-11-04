ca7ae97dd5219bfb24022bbea38bbceb82a9d8ac
Tiago Camacho <tiago.miti@gmail.com>
2025-11-04 15:45:08 +1100

    update: refinements of components

--- components/fieldset.tsx
+++ components/fieldset.tsx
@@ -1,37 +1,41 @@
 import { ReactNode } from "react";
 import { cn } from "../utils";
 
-type FieldsetProps = {
+// type FieldsetProps = {
+//   children: ReactNode;
+//   hintMessage?: string;
+//   onChange?: (value: string) => void;
+//   required?: boolean;
+//   value?: string | null | undefined;
+//   placeholder?: string;
+//   className?: string;
+//   disabled?: boolean;
+// };
+
+interface FieldsetProps extends React.InputHTMLAttributes<HTMLInputElement> {
   children: ReactNode;
   hintMessage?: string;
-  onChange?: (value: string) => void;
   required?: boolean;
-  value?: string | null | undefined;
   placeholder?: string;
   className?: string;
   disabled?: boolean;
-};
+  value?: string | number | readonly string[] | undefined;
+}
 
 export const Fieldset = ({
   children,
-  hintMessage = "* Required",
-  onChange,
-  value,
   placeholder,
   required,
   className,
   disabled = false,
+  onChange,
+  value,
 }: FieldsetProps) => {
-  const showRequiredMessage = required && !value;
+  const showRequiredMessage = required;
 
   return (
     <div className={cn("flex flex-col gap-2")}>
-      <div className="flex items-baseline justify-between">
-        <span className="font-medium">{children}</span>
-        {showRequiredMessage && (
-          <span className="validator-hint text-sm">{hintMessage}</span>
-        )}
-      </div>
+      <span className="font-medium">{children}</span>
       <div className="flex flex-col gap-0">
         <input
           type="text"
@@ -42,8 +46,8 @@ export const Fieldset = ({
             className,
             showRequiredMessage && "validator",
           )}
-          value={value ?? ""}
-          onChange={(e) => onChange?.(e.target.value)}
+          value={value}
+          onChange={onChange}
           placeholder={placeholder}
         />
       </div>
