f9915ca3c90ddcab6495acd442151a0f02b990fa
Tiago Camacho <tiago.miti@gmail.com>
2025-12-27 18:34:16 +1100

    update: style refinement

--- styles/dialog.ts
+++ styles/dialog.ts
@@ -3,8 +3,8 @@ export const dialogStyles = {
     "fixed inset-0 bg-black/30 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
   content:
     "fixed left-[50%] top-[50%] z-50 w-full max-w-xl translate-x-[-50%] translate-y-[-50%] bg-white rounded-2xl shadow-lg",
-  header: "flex items-center justify-between px-8 h-20",
-  title: "text-xl font-medium",
+  header: "flex items-center justify-between px-10 h-20",
+  title: "text-lg",
   close: "hover:bg-neutral-hint -mr-3",
-  body: "px-8 pt-0 pb-8",
+  body: "px-10 pt-0 pb-9",
 };
